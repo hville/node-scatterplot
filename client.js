c4178b61a146b4164023bf530e3b4bb6fa06d787
hville <hugov@runbox.com>
2016-10-12 15:59:44 +0800

    range selector

--- client.js
+++ client.js
@@ -5,6 +5,6 @@ function setData(json) {
 	new Dygraph(
 		document.getElementById('dygraph'),
 		JSON.parse(json),
-		{ drawPoints: true, strokeWidth: 0.0, pointSize: 3, highlightCircleSize: 5 }
+		{ drawPoints: true, strokeWidth: 0.0, pointSize: 3, highlightCircleSize: 5, showRangeSelector: true, rangeSelectorHeight: 60 }
 	)
 }
--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 const plot = require('./index.js')
 
 var set = []
-for (var i = 0; i < 500; i++) {
-	set.push([ Math.sqrt(i), i * (1 + Math.sin(i/2)) ])
+for (var i = 1; i < 500; i++) {
+	set.push([ Math.sqrt(i), i * (Math.sin(i/7) + Math.sin(i/203)/2), i * (Math.sin(i/11) + Math.sin(i/317)/2)])
 }
 
 plot(set)
